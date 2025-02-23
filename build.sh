fc9d8a34fc575b188cacaf76b88b87f416845662
John Michael <67716815+johnmikel306@users.noreply.github.com>
2025-02-23 15:44:46 +0000

    .

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # Install system dependencies
 sudo apt-get update && sudo apt-get install -y \
     portaudio19-dev \
-    python3-dev
+    python3-pyaudio
 
 # Install Python dependencies
 pip install -r requirements.txt
