d9253f318ac5fcd19fe69bd6f07bc39c0a44d0e8
Alamin AI <alamin.ibrahimfip@flexisaf.net>
2025-03-21 01:03:26 +0100

    commit

--- app/utils/jwt.py
+++ app/utils/jwt.py
@@ -5,7 +5,6 @@ import os
 from ..services import get_user_by_email
 from flask import jsonify, request
 import jwt
-import asyncio
 
 
 def token_required(f):
@@ -20,30 +19,32 @@ def token_required(f):
         if not token:
             return jsonify({'message' : 'Token is missing!'}), 401
   
-        try:
-            # decoding the payload to fetch the stored details
-            data = jwt.decode(token, os.getenv('JWT_SECRET'))
-            email = data.get('email')
-            if not email:
-                return jsonify({'message': 'Email not found in token'}), 401
-             # Fetch the user details using the email from DB and store in current_user
-            user = asyncio.run(get_user_by_email(email))  # Use asyncio.run to run the async get_user_by_email function
-            if not user:
-                return jsonify({'message' : 'User not found!'}), 401
-            
-            #store the user in the request context
-            request.current_user = user
-            logger.info(f"User {user.email} is authenticated")
-        except jwt.ExpiredSignatureError:
-            return jsonify({'message': 'Token has expired!'}), 401
-        except jwt.InvalidTokenError:
-            return jsonify({'message': 'Token is invalid!'}), 401
-        except Exception as e:
-            logger.error(f"An error occur validating the token : {str(e)}")
-            return jsonify({
-                'message': 'Token is invalid!'
-            }), 401
-        # returns the current logged in users context to the routes
+        # try:
+        # decoding the payload to fetch the stored details
+        print("Token: \n", token)
+        data = jwt.decode(token, os.getenv('JWT_SECRET'))
+        email = data.get('email')
+        if not email:
+            return jsonify({'message': 'Email not found in token'}), 401
+        print("Data: \n", data) 
+        print("Data: \n", email)   
+        user = get_user_by_email(email)  
+        if not user:
+            return jsonify({'message' : 'User not found!'}), 401
+        
+        #store the user in the request context
+        request.current_user = user
+        logger.info(f"User {user.email} is authenticated")
+        # except jwt.ExpiredSignatureError:
+        #     return jsonify({'message': 'Token has expired!'}), 401
+        # except jwt.InvalidTokenError:
+        #     return jsonify({'message': 'Token is invalid!'}), 401
+        # except Exception as e:
+        #     logger.error(f"An error occur validating the token : {str(e)}")
+        #     return jsonify({
+        #         'message': 'Token is invalid!'
+        #     }), 401
+        # # returns the current logged in users context to the routes
         return  f(*args, **kwargs)
         
     return decorated
